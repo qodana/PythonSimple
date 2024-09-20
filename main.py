e727df9feb4ca97e47f344462120922632ec2c2b
Ivan Efimov <Ivan.Efimov@jetbrains.com>
2024-09-20 12:09:12 +0300

    missed test commits

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.8
 
 
 # Press ⌃R to execute it or replace it with your code.
@@ -13,5 +13,8 @@ x=3
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+async def swagger_ui_redirect(req: Request) -> HTMLResponse:
+    return HTMLResponse()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
